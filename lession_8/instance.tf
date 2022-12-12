dc2c155b6319afe0cd0fab10de3c031b6fd53f44
Erik Goulanian <erik5goul@gmail.com>
2022-12-12 12:45:07 -0800

    hjkh

--- lession_8/instance.tf
+++ lession_8/instance.tf
@@ -4,4 +4,7 @@ resource "aws_instance" "example" {
     subnet_id              = aws_subnet.main-public-1.id
     vpc_security_group_ids = [aws_security_group.allow-ssh.id]
     key_name               = aws_key_pair.id_rsa.key_name
+    tags = {
+      "Name" = "First-Ec2"
+    }
 }
