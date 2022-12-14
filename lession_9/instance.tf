c7f34d362f1d71d16e3a1a90f44c58807fc9cc99
Erik Goulanian <erik5goul@gmail.com>
2022-12-13 18:15:44 -0800

    q

--- lession_9/instance.tf
+++ lession_9/instance.tf
@@ -20,7 +20,7 @@ resource "aws_ebs_volume" "ebs-vol-1" {
 }
 
 resource "aws_volume_attachment" "ebs-vol-1-attachment" {
-    device_name = "dev/xvdh"
+    device_name = "/dev/xvdh"
     volume_id = aws_ebs_volume.ebs-vol-1.id
     instance_id = aws_instance.example.id
     stop_instance_before_detaching = true  
