e414ab7a47c825a93cae783ecd872ecdf0611c45
Erik Goulanian <erik5goul@gmail.com>
2022-12-29 10:28:57 -0800

    dd

--- lession_10/scripts/s.sh
+++ lession_10/scripts/s.sh
@@ -25,8 +25,8 @@ if [ "`echo -n $DEVICE_FS`" == "" ] ; then
   pvcreate ${DEVICE}
   vgcreate data ${DEVICE}
   lvcreate --name volume1 -l 100%FREE data
-  mkfs.ext4 /dev/data/volume1
+  mkfs.ext4 /dev/xvdh/volume1
 fi
 mkdir -p /data
-echo '/dev/data/volume1 /data ext4 defaults 0 0' >> /etc/fstab
+echo '/dev/xvdh/volume1 /data ext4 defaults 0 0' >> /etc/fstab
 mount /data
