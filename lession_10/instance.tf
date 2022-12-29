f0c12741b2b4b171508e0a5a2dd548a0717b10dd
Erik Goulanian <erik5goul@gmail.com>
2022-12-28 16:08:47 -0800

    ye

--- lession_10/instance.tf
+++ lession_10/instance.tf
@@ -22,8 +22,8 @@ resource "aws_ebs_volume" "ebs-volume-1" {
 }
 
 resource "aws_volume_attachment" "ebs-volume-1-attachment" {
-    device_name       = var.INSTANCE_DEVICE_NAME
-    volume_id         = aws_ebs_volume.ebs-volume-1.id
-    instance_id       = aws_instance.example.id
-    skip_destroy      = true
+    device_name  = var.INSTANCE_DEVICE_NAME
+    volume_id    = aws_ebs_volume.ebs-volume-1.id
+    instance_id  = aws_instance.example.id
+    skip_destroy = true
 }
--- lession_10/scripts/volumes.sh
+++ lession_10/scripts/volumes.sh
@@ -9,7 +9,7 @@ if [ "`echo -n $DEVICE_FS`" == "" ]; then
     # wait for the device to be attached
     DEVICENAME=`echo "${DEVICE}" | awk -F '/' '{print $3}'`
     DEVICEEXISTS=''
-    while [[ -z $DEVICEEXISTS ]] 
+    while [[ -z $DEVICEEXISTS ]]
     do
         echo "checking $DEVICENAME"
         DEVICEEXISTS=`lsblk |grep "$DEVICENAME" |wc -l`
@@ -26,7 +26,7 @@ if [ "`echo -n $DEVICE_FS`" == "" ]; then
     pvcreate ${DEVICE}
     vgcreate date ${DEVICE}
     lvcreate --name volume1 -l 100%FREE data
-    mkfs.ext4 /dev/data/volume1 
+    mkfs.ext4 /dev/data/volume1
 fi
 mkdir -p /data
 echo '/dev/data/volume1 /data ext4 defaults 0 0 ' >> /etc/fstab
