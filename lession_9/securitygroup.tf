5b35de11f3b3e3e3c0d687413e9fb879ad52cf36
Erik Goulanian <erik5goul@gmail.com>
2022-12-30 22:47:24 -0800

    lfv

--- lession_9/securitygroup.tf
+++ lession_9/securitygroup.tf
@@ -17,6 +17,13 @@ resource "aws_security_group" "allow-ssh" {
         cidr_blocks = ["172.117.124.247/32"]
     }
 
+ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["108.214.21.213/32"]
+    }
+
     tags = {
       Name = "allow-ssh"
     }
