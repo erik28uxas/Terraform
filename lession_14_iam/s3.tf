ad2be22a19066ce4f992c22b3e2045f4c46b8d9c
Erik Goulanian <erik5goul@gmail.com>
2023-01-06 11:26:04 -0800

    v

--- lession_14_iam/s3.tf
+++ lession_14_iam/s3.tf
@@ -1,8 +1,13 @@
 resource "aws_s3_bucket" "b" {
     bucket = "mybucket-2827-e"
-    acl = "private"
+
 
     tags ={
         Name = "mybucket-2827-e"
     } 
+}
+
+resource "aws_s3_bucket_acl" "b-acl" {
+    acl = "private"
+  
 }
