d1d51527558e72095164068e5e156fdf4653d887
Erik Goulanian <erik5goul@gmail.com>
2023-01-13 12:38:58 -0800

    cc

--- lession_10_user_data/securitygroup.tf
+++ lession_10_user_data/securitygroup.tf
@@ -3,8 +3,6 @@ resource "aws_security_group" "allow_ssh" {
     name        = "allow-ssh"
     description = "SG that allows ssh and all egress traffic"
 
-    
-
     egress {
         from_port   = 0
         to_port     = 0
@@ -12,36 +10,26 @@ resource "aws_security_group" "allow_ssh" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 
-    dynamic "ingress" {
-        for_each = toset(var.IPS)
-        content {
-            from_port   = 22
-            to_port     = 22
-            protocol    = "tcp"
-            cidr_blocks = ingress.value
-        }
+    ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["172.117.124.247/32"]
     }
 
-    # ingress {
-    #     from_port   = 22
-    #     to_port     = 22
-    #     protocol    = "tcp"
-    #     cidr_blocks = ["172.117.124.247/32"]
-    # }
+    ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["108.214.21.213/32"]
+    }
 
-    # ingress {
-    #     from_port   = 22
-    #     to_port     = 22
-    #     protocol    = "tcp"
-    #     cidr_blocks = ["108.214.21.213/32"]
-    # }
-
-    # ingress {
-    #     from_port   = 22
-    #     to_port     = 22
-    #     protocol    = "tcp"
-    #     cidr_blocks = ["155.186.125.162/32"]
-    # }
+    ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["155.186.125.162/32"]
+    }
     
     tags = {
         Name = "allow-ssh"
