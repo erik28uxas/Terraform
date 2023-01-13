3509093e41dc47bd5d5e77984a02de8c95c6f162
Erik Goulanian <erik5goul@gmail.com>
2023-01-13 15:02:46 -0800

    f

--- lession_15_asg/autpscalingpolicy.tf
+++ lession_15_asg/autpscalingpolicy.tf
@@ -1,3 +1,4 @@
+# scale up alarm
 resource "aws_autoscaling_policy" "example-cpu-policy" {
   name                   = "example-cpu-policy"
   autoscaling_group_name = aws_autoscaling_group.example-autoscaling.name
