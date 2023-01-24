ab8c122e4e3149cfb2247f3c03e5c03da4669520
Erik Goulanian <erik5goul@gmail.com>
2023-01-24 15:58:44 -0800

    fff

--- lession_16_alb/autoscaling.tf
+++ lession_16_alb/autoscaling.tf
@@ -6,10 +6,10 @@ resource "aws_launch_configuration" "example" {
 
     user_data = <<-EOF
                 #!/bin/bash
-                yum update -y
-                yum install -y httpd
-                systemctl start httpd
-                systemctl enable httpd
+                sudo yum update -y
+                sudo yum install -y httpd
+                sudo systemctl start httpd
+                sudo systemctl enable httpd
                 echo "<h1>Greetings from $(hostname -f)</h1>" > /var/www/html/index.html
                 EOF
     
