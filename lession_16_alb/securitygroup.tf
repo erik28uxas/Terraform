b3b76c6b01c6bf7d117907ba86025c848e2e2fc2
Erik Goulanian <erik5goul@gmail.com>
2023-01-24 11:45:42 -0800

    cc

--- lession_16_alb/securitygroup.tf
+++ lession_16_alb/securitygroup.tf
@@ -16,6 +16,13 @@ resource "aws_security_group" "instance" {
         security_groups = [aws_security_group.alb.id]
     }
 
+    ingress {
+        from_port   = var.server_port
+        to_port     = var.server_port
+        protocol    = "tcp"
+        cidr_blocks = [ "0.0.0.0/0" ]
+    }
+
     
     
 }
