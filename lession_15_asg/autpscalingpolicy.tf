c88a865dc1b10fc3a0022fed2be76b399a369dc0
Erik Goulanian <erik5goul@gmail.com>
2023-01-24 09:46:21 -0800

    fgf

--- lession_15_asg/autpscalingpolicy.tf
+++ lession_15_asg/autpscalingpolicy.tf
@@ -2,7 +2,7 @@
 resource "aws_autoscaling_policy" "example-cpu-policy" {
   name                   = "example-cpu-policy"
   autoscaling_group_name = aws_autoscaling_group.example-autoscaling.name
-  adjustment_type        = "ChangeInCapacity"
+  adjustment_type        = "ChangeInCapacit y"
   scaling_adjustment     = "1"
   cooldown               = "300"
   policy_type            = "SimpleScaling"
