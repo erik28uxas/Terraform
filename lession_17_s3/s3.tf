70177c050d30ebe885036b735e4b1d0a35610008
Erik Goulanian <erik5goul@gmail.com>
2023-02-01 15:11:30 -0800

    cc

--- lession_17_s3/s3.tf
+++ lession_17_s3/s3.tf
@@ -2,7 +2,7 @@ resource "aws_s3_bucket" "bucket-tf-state" {
     bucket = "bucket-for-tf-state-from-erik-ubuntu"
 
     lifecycle {
-      prevent_destroy = true
+      prevent_destroy = false
     }
 }
 
--- lession_17_s3/vars.tf
+++ lession_17_s3/vars.tf
@@ -1,10 +1,3 @@
 variable "AWS_REGION" {
     default = "us-west-2"
 }
-
-variable "PATH_TO_PRIVATE_KEY" {
-    default = "/home/erikgoul/.ssh/id_rsa"
-}
-variable "PATH_TO_PUBLIC_KEY" {
-    default = "/home/erikgoul/.ssh/id_rsa.pub"
-}
