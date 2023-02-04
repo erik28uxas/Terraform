85eca179cbc05b824ace9994792354595431e509
Erik Goulanian <erik5goul@gmail.com>
2023-02-04 12:40:51 -0800

    b

--- lession_18_modules/prod/services/webserver-cluster/main.tf
+++ lession_18_modules/prod/services/webserver-cluster/main.tf
@@ -12,10 +12,19 @@ provider "aws" {
 #   }
 # }
 
+data "terraform_remote_state" "main-info" {
+    backend = "s3"
+
+    config = {
+        bucket         = "bucket-for-tf-state-from-erik-ubuntu"
+        key            = "global/s3/terraform.tfstate"
+        region         = "us-west-2" 
+    }
+}
 
 
 module "webserver_cluster" {
-    source = "/home/erikgoul/Documents/Terraform/lession_18_modules/modules/services/webserver-cluster"
+    source = "/home/erikgoul/Documents/Terraform/lession_18_modules/prod/services/webserver-cluster"
 
     cluster_name           = "webservers-prod"
     db_remote_state_bucket = "bucket-for-tf-state-from-erik-ubuntu"
