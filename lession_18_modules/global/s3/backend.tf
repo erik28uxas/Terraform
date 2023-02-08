2bc3d36e9aebe20c76e33bde9539b39960932733
Erik Goulanian <erik5goul@gmail.com>
2023-02-07 20:32:39 -0800

    vv

--- lession_18_modules/global/s3/backend.tf
+++ lession_18_modules/global/s3/backend.tf
@@ -1,7 +1,7 @@
 # terraform {
 #   backend "s3" {
-#     bucket         = "bucket-for-tf-state-from-erik-ubuntu"
-#     key            = "global/s3/terraform.tfstate"
-#     region         = "us-west-2" 
+#     bucket = "bucket-for-tf-state-from-erik-ubuntu"
+#     key    = "global/s3/terraform.tfstate"
+#     region = "us-west-2" 
 #   }
 # }
