ec20dd8a9d2362fb26e61bdf16c8520ab6a40582
Erik Goulanian <erik5goul@gmail.com>
2023-02-08 09:51:14 -0800

    revent

--- lession_18_modules/modules/services/webserver-cluster/alb.tf
+++ lession_18_modules/modules/services/webserver-cluster/alb.tf
@@ -1,8 +1,7 @@
 resource "aws_lb" "example" {
     name               = var.cluster_name
     load_balancer_type = "application"
-    # subnets            = data.aws_subnets.default.id
-    subnets            = ["us-west-2a", "us-west-2b", "us-west-2c"]
+    subnets            = data.aws_subnets.default.id
     security_groups    = [aws_security_group.alb.id] 
 }
 
