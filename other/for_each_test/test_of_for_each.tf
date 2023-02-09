d8533dca0ba874793976bb3783c897a912ba20ab
Erik Goulanian <erik5goul@gmail.com>
2023-02-09 12:06:31 -0800

    c v

--- other/for_each_test/test_of_for_each.tf
+++ other/for_each_test/test_of_for_each.tf
@@ -20,7 +20,7 @@ resource "aws_security_group" "allow_ssh" {
     }
     
     tags = {
-      Name = "Sg with ports ${var.custom_tags,}"
+      Name = "Sg with ports ${var.custom_tags}"
     }
     
 }
