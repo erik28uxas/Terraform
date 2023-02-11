a4ee4e40dc8999ceedeaa3b241bb3b3d82ef5753
Erik Goulanian <erik5goul@gmail.com>
2023-02-10 18:27:52 -0800

    v

--- other/for_test/for.tf
+++ other/for_test/for.tf
@@ -23,6 +23,6 @@ output "Legend_ultimates_list" {
 }
 
 output "Legend_ultimates_map" {
-    description = "Outputing from Map ${var.legens2} to Map"
+    description = "Outputing from Map 'legends_2' to Map"
     value = {for name, ult in var.legends_2 : upper(name) => uppre(ult)}
 }
