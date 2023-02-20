4f3249040c87cdc21e5900ea7b8c9c997854d225
Erik Goulanian <erik5goul@gmail.com>
2023-02-19 18:11:54 -0800

    fg

--- other/for_test/for.tf
+++ other/for_test/for.tf
@@ -55,10 +55,7 @@ variable "legends_4" {
 }
 
 output "for_string_mode" {
-    value = <<EOF
-    %{~ for i, name in var.legends_4 ~}
-        ${name}%{ if i < length(var.legends_4) - 1 },%{ else }.%{ endif }
-        %{~ endfor ~}
-EOF
+    value = "%{~ for i, name in var.legends_4 ~} ${name}%{ if i < length(var.legends_4) - 1 },%{ else }.%{ endif } %{~ endfor ~}"
+
 }
 
