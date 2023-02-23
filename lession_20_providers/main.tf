9e1767792fb9ab31e3c171cc147a93db2d728d02
Erik Goulanian <erik5goul@gmail.com>
2023-02-23 12:28:51 -0800

    b

--- lession_20_providers/main.tf
+++ lession_20_providers/main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     aws = {
-        soursource  = "hashicorp/aws"
-        versversion = "~> 4.0"
+        source  = "hashicorp/aws"
+        version = "~> 4.0"
     }
   }
 }
