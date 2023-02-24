795350c0f2c3cbab97e340e69d155ea84757e48a
Erik Goulanian <erik5goul@gmail.com>
2023-02-23 16:20:39 -0800

    updated

--- lession_20_providers/main.tf
+++ lession_20_providers/main.tf
@@ -9,12 +9,12 @@ terraform {
 
 
 provider "aws" {
-    region = "us-east-2"
+    region = "us-west-2"
     alias  = "region_1"
 }
 
 provider "aws" {
-    region = "us-west-1"
+    region = "us-east-1"
     alias  = "region_2"
 }
 
@@ -28,12 +28,49 @@ data "aws_region" "region_2" {
 }
 
 
-output "region_1" {
-    value       = data.aws_region.region_1.name
-    description = "The name of 1st region"
+data "aws_ami" "latest_amazon_ami_in_region_1" {
+    provider = aws.region_1
+
+    most_recent = true
+    owners = ["137112412989"]
+
+    filter {
+        name = "name"
+        values = ["al2022-ami-*-kernel-5.15-x86_64"]
+    }
 }
 
-output "region_2" {
-    value       = data.aws_region.region_2.name
-    description = "The name of 2nd region"
+data "aws_ami" "latest_amazon_ami_in_region_2" {
+    provider = aws.region_2
+
+    most_recent = true
+    owners = ["137112412989"]
+
+    filter {
+        name = "name"
+        values = [ "al2022-ami-*-kernel-5.15-x86_64" ]
+    }
+}
+
+
+resource "aws_instance" "region_1" {
+    provider = aws.region_1
+
+    ami           = data.aws_ami.latest_amazon_ami_in_region_1.id
+    instance_type = var.instance_type
+
+    tags = {
+        Name = "exmpl-ec2-in-Oregon"
+    }
+}
+
+resource "aws_instance" "region_2" {
+    provider = aws.region_2
+    
+    ami           = data.aws_ami.latest_amazon_ami_in_region_2.id
+    instance_type = var.instance_type
+
+    tags = {
+        Name = "exmpl-ec2-in-Virginia"
+    }
 }
---
+++ lession_20_providers/output.tf
@@ -0,0 +1,20 @@
+output "region_1" {
+    value       = data.aws_region.region_1.name
+    description = "The name of 1st region"
+}
+
+output "region_2" {
+    value       = data.aws_region.region_2.name
+    description = "The name of 2nd region"
+}
+
+
+
+
+output "instance_region_1" {
+    value = aws_instance.region_1.availability_zone
+}
+
+output "instance_region_2" {
+    value = aws_instance.region_2.availability_zone
+}
