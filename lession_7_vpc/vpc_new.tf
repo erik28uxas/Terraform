e0c0b9bb0a71c5b43e6a48c955e938f974a2afe1
Erik Goulanian <erik5goul@gmail.com>
2023-03-06 17:24:03 -0800

    fixed smthn

--- lession_7_vpc/vpc_new.tf
+++ lession_7_vpc/vpc_new.tf
@@ -12,7 +12,7 @@ resource "aws_vpc" "main_vpc" {
 }
 
 resource "aws_internet_gateway" "vpc_gw" {
-    vpc_id = aws_vpc.main-vpc.id
+    vpc_id = aws_vpc.main_vpc.id
     
     tags = {
       Name = "Main-VPC-IG"
