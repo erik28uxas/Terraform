65141daf4e07af429651b39191e64d5373213c68
Erik Goulanian <erik5goul@gmail.com>
2023-03-13 17:11:09 -0700

    vpc_1

--- lession_7_vpc/vpc_new.tf
+++ lession_7_vpc/vpc_new.tf
@@ -61,56 +61,56 @@ resource "aws_route_table_association" "public_routes" {
 }
 
 
-# # ========  Private Subnets  ========
-# resource "aws_subnet" "private_subnets" {
-#     count                   = length(var.private_subnet_cidrs)
-#     vpc_id                  = aws_vpc.main_vpc.id
-#     cidr_block              = element(var.var.private_subnet_cidrs, count.index)
-#     availability_zone       = data.aws_availability_zones.available.names[count.index]
-#     map_public_ip_on_launch = false
+# ========  Private Subnets  ========
+resource "aws_subnet" "private_subnets" {
+    count                   = length(var.private_subnet_cidrs)
+    vpc_id                  = aws_vpc.main_vpc.id
+    cidr_block              = element(var.private_subnet_cidrs, count.index)
+    availability_zone       = data.aws_availability_zones.available.names[count.index]
+    map_public_ip_on_launch = false
 
-#     tags = {
-#         Name = "Main-Private-${count.index + 1}"
-#     }
-# }
-
-
-# # ========  EIPs for NAT  ========
-# resource "aws_eip" "nat" {
-    
-#     count = length(var.private_subnet_cidrs)
-
-#     vpc = true
-  
-# }
+    tags = {
+        Name = "Main-Private-${count.index + 1}"
+    }
+}
 
 
-# # ========  NAT Gateways  ========
-# resource "aws_nat_gateway" "nat_gw" {
-#     count         = length(var.private_subnet_cidrs)
-#     allocation_id = aws_eip.nat.id
-#     subnet_id     = element(aws_subnet.private_subnets[*].id, count.index)
-#     depends_on    = [aws_internet_gateway.vpc_gw]
-# }
+# ========  EIPs for NAT  ========
+resource "aws_eip" "nat" {
+    count = length(var.private_subnet_cidrs)
+    vpc   = true
+
+    tags = {
+        Name = "EIP-for-Main-VPC-${count.index + 1}"
+    }
+}
 
 
-# # ========  NAT Route Tables  ========
-# resource "aws_route_table" "private_subnets" {
-#     vpc_id = aws_vpc.main_vpc.id
-#     route {
-#         cidr_block = var.default_cidr
-#         nat_gateway_id = aws_nat_gateway.nat_gw.id
-#     }
-#     tags = {
-#       Name = "Main-Private-NAT-${count.index + 1}"
-#     }
-# }
+# ========  NAT Gateways  ========
+resource "aws_nat_gateway" "nat_gw" {
+    count         = length(var.private_subnet_cidrs)
+    allocation_id = aws_eip.nat.id
+    subnet_id     = element(aws_subnet.private_subnets[*].id, count.index)
+    depends_on    = [aws_internet_gateway.vpc_gw]
+}
 
 
-# # ========  NAT Route Asociations for Ptivate Subnets  ========
-# resource "aws_route_table_association" "private_rouutes" {
-#     count          = length(aws_subnet.private_subnets[*].id)
-#     route_table_id = aws_route_table.private_subnets.id
-#     subnet_id      = element(aws_subnet.private_subnets[*].id, count.index)  
-# }
+# ========  NAT Route Tables  ========
+resource "aws_route_table" "private_subnets" {
+    vpc_id = aws_vpc.main_vpc.id
+    route {
+        cidr_block     = var.default_cidr
+        nat_gateway_id = aws_nat_gateway.nat_gw.id
+    }
+    tags = {
+        Name = "Main-Private-NAT-${count.index + 1}"
+    }
+}
 
+
+# ========  NAT Route Asociations for Ptivate Subnets  ========
+resource "aws_route_table_association" "private_rouutes" {
+    count          = length(aws_subnet.private_subnets[*].id)
+    route_table_id = aws_route_table.private_subnets.id
+    subnet_id      = element(aws_subnet.private_subnets[*].id, count.index)  
+}
