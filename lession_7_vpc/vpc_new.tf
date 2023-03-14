82fa59103bb2892e3115f3e7f4add6a9189b7884
Erik Goulanian <erik5goul@gmail.com>
2023-03-13 17:51:54 -0700

    cv

--- lession_7_vpc/vpc_new.tf
+++ lession_7_vpc/vpc_new.tf
@@ -89,7 +89,7 @@ resource "aws_eip" "nat" {
 # ========  NAT Gateways  ========
 resource "aws_nat_gateway" "nat_gw" {
     count         = length(var.private_subnet_cidrs)
-    allocation_id = aws_eip.nat.id
+    allocation_id = element(aws_eip.nat.*.id, count.index)
     subnet_id     = element(aws_subnet.private_subnets.*.id, count.index)
     depends_on    = [aws_internet_gateway.vpc_gw]
 }
