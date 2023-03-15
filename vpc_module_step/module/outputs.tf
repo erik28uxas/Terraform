fc39a849c328ba1133ccd0487970a618910fb7a0
Erik Goulanian <erik5goul@gmail.com>
2023-03-15 15:45:25 -0700

    added IGW

--- vpc_module_step/module/outputs.tf
+++ vpc_module_step/module/outputs.tf
@@ -7,3 +7,10 @@ output "vpc_cidr" {
   description = "The ID of the VPC"
   value       = aws_vpc.main_vpc.cidr_block
 }
+
+output "igw_id" {
+  description = "The ID of the Internet Gateway"
+#   value       = try(aws_internet_gateway.vpc_gw[0].id, "")
+  value       = aws_internet_gateway.vpc_gw.id
+}
+
