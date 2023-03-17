d7499a2a069531124851cfe428ad6580f2dcb6da
Erik Goulanian <erik5goul@gmail.com>
2023-03-17 13:03:07 -0700

    fix_3.1

--- vpc_module_step/env/main.tf
+++ vpc_module_step/env/main.tf
@@ -32,6 +32,10 @@ module "vpc" {
   private_subnet_cidrs = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
 
 
+  enable_nat_gateway = true
+  single_nat_gateway = false
+  external_nat_ip_ids = "${aws_eip.nat.*.id}"
+
 #   public_subnet_names = "Main Public Subnet"
 
   public_subnet_tags = {
--- vpc_module_step/module/vars.tf
+++ vpc_module_step/module/vars.tf
@@ -54,7 +54,7 @@ variable "single_nat_gateway" {
   type        = bool
   default     = false
 }
-
+#############
 variable "one_nat_gateway_per_az" {
   description = "Should be true if you want only one NAT Gateway per availability zone. Requires `var.azs` to be set, and the number of `public_subnets` created to be greater than or equal to the number of availability zones specified in `var.azs`."
   type        = bool
@@ -72,7 +72,7 @@ variable "external_nat_ip_ids" {
   type        = list(string)
   default     = []
 }
-
+##############
 variable "nat_gateway_destination_cidr_block" {
   description = "Used to pass a custom destination route for private NAT Gateway. If not specified, the default 0.0.0.0/0 is used as a destination route."
   type        = string
