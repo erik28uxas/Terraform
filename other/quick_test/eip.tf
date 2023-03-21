2114d283b698402cfbd656d33d2a20232f04ef79
Erik Goulanian <erik5goul@gmail.com>
2023-03-21 15:05:19 -0700

    kjh

--- other/quick_test/eip.tf
+++ other/quick_test/eip.tf
@@ -1,8 +1,29 @@
-resource "aws_eip" "nat" {
-    count = 3
-    vpc   = true
+# resource "aws_eip" "nat" {
+#     count = 3
+#     vpc   = true
 
-    tags = {
-        Name = "EIP-for-Main-VPC-${count.index + 1} "
-    }
+#     tags = {
+#         Name = "EIP-for-Main-VPC-${count.index + 1} "
+#     }
+# }
+
+
+resource "aws_instance" "example" {
+  ami = "ami-0688ba7eeeeefe3cd"
+  instance_type = "t2.micro"
+
+  ebs_block_device {
+    device_name = "sda2"
+    volume_size = 2
+  }
+  
+  ebs_block_device {
+    device_name = "sda3"
+    volume_size = 4
+  } 
+}
+
+
+output "ebs_blocks" {
+    value = aws_instance.example.ebs_block_device[*].id  
 }
