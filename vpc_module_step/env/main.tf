31f8f0bb932faec077af713becdcc01611eeb1b0
Erik Goulanian <erik5goul@gmail.com>
2023-04-03 22:28:30 -0700

    nat_4.3.12

--- vpc_module_step/env/main.tf
+++ vpc_module_step/env/main.tf
@@ -27,7 +27,7 @@ module "vpc" {
   source = "/home/erikgoul/Documents/Terraform/vpc_module_step/module"
 
 
-  azs                  = ["${local.region}a", "${local.region}b", "${local.region}c"]
+  # azs                  = ["${local.region}a", "${local.region}b", "${local.region}c"]
   # public_subnet_cidrs  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   private_subnet_cidrs = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
 
