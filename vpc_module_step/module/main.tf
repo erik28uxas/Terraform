d92e582a0bfea2a9b14ee9a3cd2c4f47e1a3515e
Erik Goulanian <erik5goul@gmail.com>
2023-04-04 16:30:08 -0700

    nat_4.4.1

--- vpc_module_step/module/main.tf
+++ vpc_module_step/module/main.tf
@@ -103,9 +103,9 @@ resource "aws_subnet" "public_subnets" {
 
   vpc_id                  = local.vpc_id
   cidr_block              = element(concat(var.public_subnet_cidrs, [""]), count.index)
-  # availability_zone       = length(regexall("^[a-z]{2}-", element(var.azs, count.index))) > 0 ? element(var.azs, count.index) : null
-  availability_zone       = element(var.azs, count.index)
-  # availability_zone_id    = length(regexall("^[a-z]{2}-", element(var.azs, count.index))) == 0 ? element(var.azs, count.index) : null
+  availability_zone       = length(regexall("^[a-z]{2}-", element(var.azs, count.index))) > 0 ? element(var.azs, count.index) : null
+  # availability_zone       = element(var.azs, count.index)
+  availability_zone_id    = length(regexall("^[a-z]{2}-", element(var.azs, count.index))) == 0 ? element(var.azs, count.index) : null
   map_public_ip_on_launch = var.map_public_ip_on_launch
 
   tags = merge(
@@ -128,9 +128,9 @@ resource "aws_subnet" "private_subnets" {
 
   vpc_id               = local.vpc_id
   cidr_block           = element(var.private_subnet_cidrs, count.index)
-  # availability_zone    = length(regexall("^[a-z]{2}-", element(var.azs, count.index))) > 0 ? element(var.azs, count.index) : null
-  availability_zone    = element(var.azs, count.index)
-  # availability_zone_id = length(regexall("^[a-z]{2}-", element(var.azs, count.index))) == 0 ? element(var.azs, count.index) : null
+  availability_zone    = length(regexall("^[a-z]{2}-", element(var.azs, count.index))) > 0 ? element(var.azs, count.index) : null
+  # availability_zone    = element(var.azs, count.index)
+  availability_zone_id = length(regexall("^[a-z]{2}-", element(var.azs, count.index))) == 0 ? element(var.azs, count.index) : null
  
   tags = merge(
     {
