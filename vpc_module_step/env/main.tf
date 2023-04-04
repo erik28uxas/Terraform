fa7d7235c30f20d80713d2c9e2179c780635617e
Erik Goulanian <erik5goul@gmail.com>
2023-04-03 17:53:23 -0700

    nat_2.2

--- vpc_module_step/env/main.tf
+++ vpc_module_step/env/main.tf
@@ -34,8 +34,7 @@ module "vpc" {
 
   enable_nat_gateway     = true
   single_nat_gateway     = false
-  reuse_nat_ips          = false
-  one_nat_gateway_per_az = true 
+  one_nat_gateway_per_az = false 
   external_nat_ip_ids    = "${aws_eip.nat.*.id}"
 
 #   public_subnet_names = "Main Public Subnet"
