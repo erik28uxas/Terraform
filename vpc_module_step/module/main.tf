00c8f28aefb4c9c2fa03cfaa76872c77bc714911
Erik Goulanian <erik5goul@gmail.com>
2023-04-03 21:06:12 -0700

    nat_4.1

--- vpc_module_step/module/main.tf
+++ vpc_module_step/module/main.tf
@@ -3,7 +3,7 @@ locals {
     length(var.private_subnet_cidrs),
     # length(var.database_subnets),
   )
-  nat_gateway_count = var.single_nat_gateway ? 1 : var.one_nat_gateway_per_az ? length(var.azs) : local.max_subnet_length
+  # nat_gateway_count = var.single_nat_gateway ? 1 : var.one_nat_gateway_per_az ? length(var.azs) : local.max_subnet_length
   
   # Use `local.vpc_id` to give a hint to Terraform that subnets should be deleted before secondary CIDR blocks can be free!
   vpc_id = try(aws_vpc_ipv4_cidr_block_association.this[0].vpc_id, aws_vpc.main_vpc[0].id, "")
