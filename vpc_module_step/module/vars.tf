1a825cbfd310c879452cd4b7d3e53a594c90f117
Erik Goulanian <erik5goul@gmail.com>
2023-04-03 20:08:01 -0700

    nat_3.3.3

--- vpc_module_step/module/vars.tf
+++ vpc_module_step/module/vars.tf
@@ -1,7 +1,7 @@
 variable "create_vpc" {
   description = "Controls if VPC should be created (it affects almost all resources)"
   type        = bool
-  default     = false
+  default     = true
 }
 
 
