6e78eff1500a635607b0390c7a48510f74ff8726
Erik Goulanian <erik5goul@gmail.com>
2023-04-03 20:12:21 -0700

    nat_3.3.6

--- vpc_module_step/module/vars.tf
+++ vpc_module_step/module/vars.tf
@@ -105,7 +105,7 @@ variable "azs" {
 variable "public_subnet_cidrs" {
   description = "Public Subnets CIDRs"
   type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  default     = []
 }
 
 variable "private_subnet_cidrs" {
