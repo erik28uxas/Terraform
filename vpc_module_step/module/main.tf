bcfcc9f0ebb07f1ed3c06fa0e93b75497e4a80db
Erik Goulanian <erik5goul@gmail.com>
2023-04-03 20:20:10 -0700

    nat_3.3.10

--- vpc_module_step/module/main.tf
+++ vpc_module_step/module/main.tf
@@ -28,7 +28,7 @@ resource "aws_vpc_ipv4_cidr_block_association" "this" {
   count = local.create_vpc && length(var.secondary_cidr_blocks) > 0 ? length(var.secondary_cidr_blocks) : 0
 
   # Do not turn this into `local.vpc_id`
-  vpc_id = aws_vpc.this[0].id
+  vpc_id = aws_vpc.main_vpc[0].id
 
   cidr_block = element(var.secondary_cidr_blocks, count.index)
 }
