1df8cb211b8f40d453aceba9b693f4eaead910d1
Erik Goulanian <erik5goul@gmail.com>
2023-04-06 15:48:12 -0700

    nat_4.4.12

--- vpc_module_step/env/main.tf
+++ vpc_module_step/env/main.tf
@@ -92,12 +92,12 @@ module "vpc" {
   #   Info = "EIP-in-${local.region}c"
   # }
 
-  nat_gateway_tags = {
-    Info = "NAT-GW-for-${local.region}a"
-    Info = "NAT-GW-for-${local.region}b"
-    Info = "NAT-GW-for-${local.region}c"
+  # nat_gateway_tags = {
+  #   Info = "NAT-GW-for-${local.region}a"
+  #   Info = "NAT-GW-for-${local.region}b"
+  #   Info = "NAT-GW-for-${local.region}c"
 
-  }
+  # }
 
   tags = local.tags
 
