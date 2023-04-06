dbe098c41838bff2099484794fbe233ddf5618b0
Erik Goulanian <erik5goul@gmail.com>
2023-04-06 15:14:39 -0700

    nat_4.4.8

--- vpc_module_step/module/main.tf
+++ vpc_module_step/module/main.tf
@@ -174,7 +174,7 @@ resource "aws_nat_gateway" "main" {
   count = local.create_vpc && var.enable_nat_gateway ? local.nat_gateway_count : 0
 
   allocation_id = element(local.nat_gateway_ips, var.single_nat_gateway ? 0 : count.index)
-  subnet_id     = element(aws_subnet.public_subnets[*], var.single_nat_gateway ? 0 : count.index)
+  subnet_id     = element(aws_subnet.public_subnets[*].id, var.single_nat_gateway ? 0 : count.index)
 
   depends_on = [aws_internet_gateway.vpc_gw]
 
